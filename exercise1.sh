b20a2d56cc55c3f230f1b30e2446a5a4dbd307d5
Suresh Andem <sureshandem@sureshs-mbp.home>
2021-07-30 01:23:55 +0100

    sum of the intergers

---
+++ exercise1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Hai this is suresh andem
+# purpose : to practice and to add  the integers
+# creation date : 29 jul 2021
+
+array=(1 , 2 , 3 , 4 , 5)
+
+a=1
+b=2
+c=3
+d=4
+e=5
+
+sum=$(($a + $b + $c + $d + $e))
+
+echo "THE SUM IS : $sum"
+
---
+++ exercise2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Hai this is suresh andem
+# purpose : To add the integers
+# creation date : 29  jul 2021
+
+read -p "Enter the first number : " num1
+read -p "Enter the second number : " num2
+read -p "Enter the third number : " num3
+read -p "Enter the fourth number : " num4
+read -p "Enter the fifth number : " num5
+
+
+sum=$(($num1 + $num2 + $num3 + $num4 + $num5))
+
+echo "THE SUM IS : $sum"
+
